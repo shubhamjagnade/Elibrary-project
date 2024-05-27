48464d74bc5b21637ca7873604b2f91095aa32b2
shubhamjagnade <shubhamjagu513@gmail.com>
2024-05-27 14:01:44 +0530

    Add files via upload
    Elibrary database

---
+++ Login.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Login : System.Web.UI.Page
+    {
+
+        string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        //user login
+        protected void Button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con= new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                SqlCommand cmd = new SqlCommand( "select * from member_master_table" +
+                    "where member_id='"+TextBox1.Text.Trim()+"' AND password='"+ TextBox2.Text.Trim() + "'", con);
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows) 
+                {
+                      while (dr.Read()) 
+                    {
+                        Response.Write("<script>alert('"+dr.GetValue(8).ToString()+"');</script>");
+                    }
+                }
+                else
+                {
+                    Response.Write("<script>alert('Invalid User');</script>");
+                }
+
+            }
+            catch(Exception ex)
+            { 
+            
+            
+            }
+           // Response.Write("<script>alert('Button click');</script>");
+        }
+
+
+
+
+
+        //user define functions
+    }
+}
